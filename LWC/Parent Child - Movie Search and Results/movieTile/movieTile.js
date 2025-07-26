e36700374a5dc64aefa9bad699b0fcc7bbf619f1
abdullahquddusi <abdullahquddusi@gmail.com>
2025-07-26 19:12:01 +0500

    Delete LWC/Parent Child - Movie Search and Results directory

--- LWC/Parent Child - Movie Search and Results/movieTile/movieTile.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class MovieTile extends LightningElement {
-    @api movie;
-
-    connectedCallback() {
-    console.log('Movie passed to tile:', this.movie);
-}
-}
